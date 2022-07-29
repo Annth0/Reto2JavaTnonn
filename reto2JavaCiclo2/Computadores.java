3ed81666b79d9aea3fa55da437d0eae2e9760b21
Anthonne <luangapa@gmail.com>
2022-07-29 15:23:53 -0500

    El reto 2 con los test segunda parte

--- reto2JavaCiclo2/Computadores.java
+++ reto2JavaCiclo2/Computadores.java
@@ -174,8 +174,7 @@ public class PrecioTotal {
     }
 }
 
-
-//
+// test1
 //Computadores computadores[] = new Computadores[6];
 //computadores[0] = new Computadores(150.0, 70, 'A');
 //computadores[1] = new ComputadoresMesa(70.0, 40);
@@ -187,3 +186,13 @@ public class PrecioTotal {
 //\tsolucion1.mostrarTotales();
 //\tSystem.out.println();
 
+
+//Test 2
+//Computadores computadores[] = new Computadores[4];
+//computadores[0] = new Computadores(60.0, 10, 'D');
+//computadores[1] = new ComputadoresMesa(300.0, 40, 'Z', 40);
+//computadores[2] = new ComputadoresPortatiles(50.0, 10, 'A', 70, false);
+//computadores[3] = new Computadores(50.0, 10);
+//PrecioTotal solucion1 = new PrecioTotal(computadores);
+//solucion1.mostrarTotales();
+//System.out.println();
